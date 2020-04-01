031470d43aa49263de7885a048500e6baa204328
Malik <malikmukhtar63@gmail.com>
2020-04-01 17:50:20 +0100

    added the marked markdown compiler

--- src/App.js
+++ src/App.js
@@ -1,32 +1,77 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import marked from "marked";
+
+const initailState = `
+This is a paragraph
+
+**This is a bolded text**
+
+> Block Quotes!
+
+# Heading
+## Heading 2
+### Heading 3
+
+- list item 1
+- list item 2
+
+[Visit my website](https://malikmukhtar.com)
+
+This is an Inline \`<div></div>\`
+
+This is a block of code
+
+\`\`\`
+  let x = 1;
+  let y = 2;
+  let z = x + y;
+  console.log(z);
+\`\`\`
+
+![React](https://goo.gl/Umyytc)
+
+`;
 
 class App extends React.Component {
   state = {
-    markdown: ""
+    text: initailState
   };
 
   handleChange = e => {
     this.setState({
-      markdown: e.value.target
+      text: e.target.value
     });
   };
 
   render() {
-    const { markdown } = this.state;
+    const { text } = this.state;
+
+    const markdown = marked(text);
+
     return (
-      <div className="">
-        <h1>Convert your Markdown</h1>
+      <div className="container">
+        <h2 className="text-center m-4">Convert your Markdown</h2>
         <div className="row">
           <div className="col-6">
+            <h6 className="text-center">Enter your markdown here:</h6>
             <textarea
+              className="form-control p-2"
               id="editor"
-              value="markdown"
+              value={text}
               onChange={this.handleChange}
             />
           </div>
-          <div className="col-6" id="preview"></div>
+          <div className="col-6">
+            <h6 className="text-center">See the results:</h6>
+            <div
+              id="preview"
+              className="preview rounded"
+              dangerouslySetInnerHTML={{ __html: markdown }}
+            ></div>
+          </div>
         </div>
       </div>
     );
